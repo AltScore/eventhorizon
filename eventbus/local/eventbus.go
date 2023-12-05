9431cb34843688f3a9723cb14bca942143ad7345
Charly Fau <carlos.fau@altscore.ai>
2023-12-05 14:04:11 -0300

    Fix typos

--- eventbus/local/eventbus.go
+++ eventbus/local/eventbus.go
@@ -99,7 +99,7 @@ func (b *EventBus) HandleEvent(ctx context.Context, event eh.Event) error {
 }
 
 // AddHandler implements the AddHandler method of the eventhorizon.EventBus interface.
-func (b *EventBus) AddHandler(ctx context.Context, m eh.EventMatcher, h eh.EventHandler) error {
+func (b *EventBus) AddHandler(_ context.Context, m eh.EventMatcher, h eh.EventHandler) error {
 	if m == nil {
 		return eh.ErrMissingMatcher
 	}
@@ -219,7 +219,7 @@ func (g *Group) channel(id string) <-chan []byte {
 	return ch
 }
 
-func (g *Group) publish(ctx context.Context, b []byte) error {
+func (g *Group) publish(_ context.Context, b []byte) error {
 	g.busMu.RLock()
 	defer g.busMu.RUnlock()
 
